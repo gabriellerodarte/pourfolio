b49e7eaa36bf382e70ae2518073be2c1b9cb5cfc
Gabrielle <ms.grodarte@gmail.com>
2025-06-03 13:30:58 -0700

    add initial routes with createBrowserRouter

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <div>
+      <header>
+
+      </header>
+      <main>
+        <Outlet/>
+      </main>
+    </div>
+  )
 }
 
 export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,16 @@
+import App from "./components/App";
+
+
+const routes = [
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+
+            }
+        ]
+    }
+]
+
+export default routes
