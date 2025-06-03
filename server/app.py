2e9ee8cddc35ba11b538bbdd8fd3667afe5d0b65
Gabrielle <ms.grodarte@gmail.com>
2025-06-03 12:15:40 -0700

    add signup view

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
@@ -12,6 +12,29 @@ from config import app, db, api
 from models import User
 
 # Views go here!
+class Signup(Resource):
+
+    def post(self):
+        json = request.get_json()
+
+        if 'username' not in json or 'password' not in json:
+            return {'error':'Username and password are requried.'}, 400
+
+        try:
+            user = User(
+                username=json['username']
+            )
+            user.password_hash = json['password']
+            db.session.add(user)
+            db.session.commit()
+            session['user_id'] = user.id
+            return user.to_dict(exclude=['_password_hash']), 201
+        except IntegrityError:
+            db.session.rollback()
+            return {'error': 'Username already exists.'}, 409
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 500
 
 @app.route('/')
 def index():
@@ -20,4 +43,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
